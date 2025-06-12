650880b96c02c8515f75b041d4418bde9908aea8
aravinth.exe <aravinth.exe@gmail.com>
2025-06-12 13:58:42 +0530

    updated code

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 @app.get("/check/")
 async def check():
-    print("[LOG] Health check endpoint accessed", flush=True)
+    print("[LOG] Health check endpoint accessed ", flush=True)
     return {"status": "OK"}
 
 @app.post("/upload/")
